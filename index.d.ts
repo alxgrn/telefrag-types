0666c5ee4fb84baa67604668f932889b86440ba9
alxgrn <alxgrn@users.noreply.github.com>
2025-09-30 19:03:37 +0300

    TUser can_jam

--- index.d.ts
+++ index.d.ts
@@ -29,6 +29,7 @@ export type TUser = {
     email: string;
     password?: string; // Мы всегда удаляем пароль из выдачи
     is_root: boolean;
+    can_jam: boolean; // Может создавать джем
     can_invite: boolean; // Может приглашать пользователей без ограничений
     restore_code?: string | null; // Мы всегда удаляем код из выдачи
     created: string;
